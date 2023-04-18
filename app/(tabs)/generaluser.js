31634de464b95503bf17e2fd28c08e057688948d
Helix320 <64866893+Helix320@users.noreply.github.com>
2023-04-17 23:57:54 -0600

    jijos ya no se

---
+++ app/(tabs)/generaluser.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import { Text, View, StyleSheet, Image, TextInput } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Bienvenido</Text>
+      <Text style={styles.subtitulo}>Que desea hacer?</Text>
+      <View style={styles.buttons}>
+        <TouchableOpacity style={styles.lg}>
+          <LinearGradient
+            colors={["#2a2a2a", "#000000", "#2a2a2a"]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 1 }}
+            style={styles.button}
+          >
+            <Text style={styles.textButton}>Nuevo registro</Text>
+          </LinearGradient>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.lg}>
+          <LinearGradient
+            colors={["#2a2a2a", "#000000", "#2a2a2a"]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 1 }}
+            style={styles.button}
+          >
+            <Text style={styles.textButton}>Ver registros</Text>
+          </LinearGradient>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  //funcion como tipo css
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f1f1f1",
+  },
+  titulo: {
+    fontSize: 50,
+    fontWeight: "bold",
+    color: "#000000",
+  },
+  subtitulo: {
+    fontSize: 30,
+    marginTop: 20,
+    color: "#34434d",
+  },
+  buttons: {
+    flexDirection: "row",
+  },
+  button: {
+    width: "80%",
+    height: 50,
+    borderRadius: 10,
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textButton: {
+    fontSize: 14,
+    color: "#ffffff",
+    fontWeight: "bold",
+  },
+  lg: {
+    alignItems: "center",
+    width: 200,
+    marginTop: 60,
+  },
+});
+
+export default App;
---
+++ app/(tabs)/index.js
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import { TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TextInput,
+} from "react-native";
+import icon from "../assets/caricon.png";
+import { LinearGradient } from 'expo-linear-gradient';
+import { Link } from "expo-router";
+
+const App = () => {
+  const [userName, setUserName] = useState('');
+  const [pwd, setpwd] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <Image source={icon} style={styles.imageicon} />
+
+      <Text style={styles.titulo}>Iniciar Sesion</Text>
+
+      <TextInput
+          value={userName}
+          onChangeText={(userName) => setUserName(userName)}
+          placeholder={'Usuario'}
+          style={styles.input}
+        />
+      
+      <TextInput
+          value={pwd}
+          onChangeText={(pwd) => setpwd(pwd)}
+          placeholder={'Contraseña'}
+          style={styles.input}
+        />
+
+      <Text style={styles.forgotPassword}>Olvidaste tu contraseña?</Text>
+
+      <TouchableOpacity style={styles.lg}>
+      <LinearGradient
+        colors={['#2a2a2a','#000000', '#2a2a2a']}
+        start={{x:0, y:0}}
+        end={{x:1, y:1}}
+        style={styles.button}
+        >
+        <Link href="generaluser" style={styles.textButton}>Iniciar sesion</Link>
+      </LinearGradient>
+      </TouchableOpacity>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  //funcion como tipo css
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f1f1f1"
+  },
+
+  imageicon: {
+    marginBottom: 20,
+    borderRadius: 5,
+    width: 200,
+    height: 100,
+  },
+
+  titulo: {
+    fontSize:50,
+    fontWeight: "bold",
+    color: "#34434d"
+  },
+  input: {
+    width: '80%',
+    height: 50,
+    padding: 10,
+    marginTop: 20,
+    marginBottom: 10,
+    borderRadius: 30,
+    backgroundColor: "#ffffff",
+    paddingStart: 30,
+  },
+  forgotPassword: {
+      fontSize: 14,
+      color: "gray",
+      marginTop: 10,
+  },
+  button: {
+    width: '80%',
+    height: 50,
+    borderRadius: 25,
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textButton:{
+    fontSize: 14,
+    color: "#ffffff",
+    fontWeight: "bold",
+  },
+  lg:{
+    alignItems: "center",
+    width:200,  
+    marginTop: 60,
+  } 
+});
+
+export default App;
